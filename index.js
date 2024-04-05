009657942f9a64442ddabf97596e986764ee35e2
Rupeshbabu <nrupesh08@gmail.com>
2024-04-05 18:06:10 +0530

    add prettier file

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const slugify = require('slugify');
+
 const replaceTemplate = require('./modules/replaceTemplate');
 
 ////////////////////////
@@ -29,48 +31,47 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 
 const data = fs.readFileSync(`${__dirname}/dev_data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
-    
-const server = http.createServer((req, res) =>{
-    // const pathname = req.url;
-    const {query, pathname} = url.parse(req.url, true);
 
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
 
-    //OVERVIEW PAGE
-    if(pathname === '/' || pathname === '/overview'){
-        res.writeHead(200, { 'Content-Type':'text/html'});
+const server = http.createServer((req, res) => {
+  // const pathname = req.url;
+  const { query, pathname } = url.parse(req.url, true);
 
-        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
-        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
-        res.end(output);
+  //OVERVIEW PAGE
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+
+    const cardsHtml = dataObj.map((el) => replaceTemplate(tempCard, el)).join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    res.end(output);
 
     //PRODUCT PAGE
-    }else if(pathname === '/product'){
-        const product = dataObj[query.id];
-        res.writeHead(200, { 'Content-Type':'text/html'});
-        const output = replaceTemplate(tempProduct, product)
-        res.end(output);
+  } else if (pathname === '/product') {
+    const product = dataObj[query.id];
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
 
     //API
-    }else if(pathname === '/api') {
-        res.writeHead(200, { 'Content-Type':'application/json'});
-        res.end(data);
-        // fs.readFile(`${__dirname}/dev_data/data.json`, 'utf-8', (err, data)=>{
-        //     const productData = JSON.parse(data); 
-        // });
-    
+  } else if (pathname === '/api') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(data);
+    // fs.readFile(`${__dirname}/dev_data/data.json`, 'utf-8', (err, data)=>{
+    //     const productData = JSON.parse(data);
+    // });
+
     //NOT FOUND
-    }else{
-        res.writeHead(404, {
-            'Content-Type':'text/html',
-            'my-own-header':'hellow-world'
-        });
-        res.end('<h1>Page not found :(</h1>');
-    }
+  } else {
+    res.writeHead(404, {
+      'Content-Type': 'text/html',
+      'my-own-header': 'hellow-world',
+    });
+    res.end('<h1>Page not found :(</h1>');
+  }
 });
 
-
-server.listen(8000, '127.0.0.1', () =>{
-    console.log('Server has been started to 8000');
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Server has been started to 8000');
 });
-
-
